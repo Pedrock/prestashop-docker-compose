cee55c1767464f948f12aa1be737a0d7d496bf45
Pedro Câmara <camara-pedro@hotmail.com>
2018-07-26 09:24:29 +0000

    Fixed autobackup cron job

--- docker-images/prestashop-autobackup/entrypoint.sh
+++ docker-images/prestashop-autobackup/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ln -sf /proc/$$/fd/1 /var/log/cron.log
-echo "$BACKUP_SCHEDULE /app/duplicacy-autobackup.sh backup &> /var/log/cron.log" | crontab
+echo "$BACKUP_SCHEDULE /app/duplicacy-autobackup.sh backup >> /var/log/cron.log 2>&1" | crontab
 /app/duplicacy-autobackup.sh init
 
 if [[ $BACKUP_IMMEDIATLY == "yes" ]] || [[ $BACKUP_IMMEDIATELY == "yes" ]]; then # two spellings for retro-compatibility
